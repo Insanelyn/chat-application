16fd5f5decce085fc7f1fafb4e86d9d1dde615e4
JohanSonmark <johansonmark@hotmail.com>
2018-03-11 16:22:58 +0100

    remove the problem

--- scripts/login.js
+++ scripts/login.js
@@ -1,26 +1,6 @@
 
 
-var provider = new firebase.auth.GoogleAuthProvider();
 
-$("#google-login").click(() => {
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-    window.location.assign("views/chat.html");
-}).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-});
-});
 
 $("#login-email-password").click((event) => {
     event.preventDefault();
