9b97063963f90a91996c661dffd82f29724e1104
JohanSonmark <johansonmark@hotmail.com>
2018-03-12 14:48:30 +0100

    inte klart

--- scripts/login.js
+++ scripts/login.js
@@ -7,13 +7,28 @@ $("#login-email-password").click((event) => {
 var email = $("#email").val();
 var password = $("#password").val();
 
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    console.log("Error code", errorCode);
-    var errorMessage = error.message;
-    console.log("Error message", errorMessage);
-    // ...
-});
-window.location.assign("views/chat.html")
-});
+firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+        }
+        if (errorCode === 'auth/user-not-found') {
+            alert('User not found!.');
+        }
+        if (errorCode === 'auth/user-disabled') {
+            alert('User disabled.');
+        }
+        if (errorCode === 'auth/invalid-email') {
+            alert('Invalid email.');
+        }
+        else {
+            alert(errorMessage);
+        }
+        console.log(error);
+
+    });
+window.location.assign("views/chat.html");
+})
--- scripts/register.js
+++ scripts/register.js
@@ -4,12 +4,16 @@ $("#email-password-create").click((event) => {
     event.preventDefault();
     let email = $("#email").val();
     let password = $("#password").val();
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch(function(error) {
         // Handle Errors here.
-        let errorCode = error.code;
-        console.log("Error code", errorCode);
-        let errorMessage = error.message;
-        console.log("Error message", errorMessage);
-        // ...
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        if (errorCode == 'auth/weak-password') {
+            alert('The password is too weak.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
     });
 });
