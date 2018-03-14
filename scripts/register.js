5685e2d9cf3cec3fdad306e980df2f0352afac0f
Evelyn Jepsen <evelynjepsen@gmail.com>
2018-03-14 12:21:07 +0100

    Fixed formatting

--- scripts/register.js
+++ scripts/register.js
@@ -7,14 +7,15 @@ let password = $("#password").val();
 firebase.auth().createUserWithEmailAndPassword(email, password)
     .catch(function(error) {
         // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
+        let errorCode = error.code;
+        let errorMessage = error.message;
         if (errorCode == 'auth/weak-password') {
             alert('The password is too weak.');
         } else {
             alert(errorMessage);
         }
         console.log(error);
-    });
-    
+    })
 });
+$("#formReg").trigger('reset');
+
