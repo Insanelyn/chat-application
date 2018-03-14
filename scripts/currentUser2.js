416aae6597c11c57365ae72196bdb2bcccae9247
Matilda Hermansson <hermanssonmatilda@gmail.com>
2018-03-14 17:06:22 +0100

    CURRENT USER KLAR - bugg login klar

---
+++ scripts/currentUser2.js
@@ -0,0 +1,19 @@
+let body2 = document.getElementsByTagName("body")[0];
+let loggedIn2 = document.createElement("div");
+let loggedInHeader2 = document.createElement("h3");
+loggedInHeader2.innerHTML = "";
+loggedIn2.innerHTML = sessionStorage.getItem("");
+body2.appendChild(loggedInHeader2);
+body2.appendChild(loggedIn2);
+
+$("#login-email-password").click((event) => {
+    event.preventDefault();
+    let email = $("#email").val();
+    let password = $("#password").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(() => {
+            window.sessionStorage.setItem('loggedIn2', email);
+            window.location.assign("../views/index.html");
+        } )
+});
